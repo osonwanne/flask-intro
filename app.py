43872fb2a93bfd1ba1d173319d10effc86f125cd
osonwanne <nosonwan@gmail.com>
2015-07-26 00:17:15 -0700

    Part 2 - Creating a login page

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-from flask import Flask, render_template
+# import the Flask class from the flask module
+from flask import Flask, render_template, redirect, url_for, request
 
+# create the application object
 app = Flask(__name__)
 
-
+# use decorators to link the function to a url
 @app.route('/')
 def home():
-    return "Hello, Chance!"
-
+    return "Hello, Chance!" #return a string
 
 @app.route('/welcome')
 def welcome():
-    return render_template("welcome.html")
+    return render_template("welcome.html") # render a template
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 if __name__  == '__main__':
     app.run(debug=True)
