f8b4006c794982900c92209c87df5b14d431dfe1
osonwanne <nosonwan@gmail.com>
2015-07-26 21:08:52 -0700

    unit test for each functionality

--- test.py
+++ test.py
@@ -15,5 +15,32 @@ class FlaskTestCase(unittest.TestCase):
         response = tester.get('/login', content_type='html/text')
         self.assertTrue(b'Please login' in response.data)
 
+    # Ensure login behaves correctly given the correct credentials
+    def test_correct_login(self):
+        tester = app.test_client(self)
+        response = tester.post('/login', data=dict(username="admin", password="admin"), follow_redirects=True)
+        self.assertIn(b'You were just logged in!', response.data)
+
+    # Ensure login behaves correctly given the incorrect credentials
+    def test_incorrect_login(self):
+        tester = app.test_client(self)
+        response = tester.post('/login', data=dict(username="blaa", password="admin"), follow_redirects=True)
+        self.assertIn(b'Invalid credentials. Please try again.', response.data)
+
+    # Ensure logout behaves correctly
+    def test_logout(self):
+        tester = app.test_client(self)
+        tester.post('/login', data=dict(username="blaa", password="admin"), follow_redirects=True)
+        response = tester.get('/logout', follow_redirects=True)
+        self.assertIn(b'You were just logged out!', response.data)
+
+"""
+    # Ensure that the main page requires login
+    def test_main_route_requires_login(self):
+        tester = app.test_client(self)
+        response = tester.get('/', follow_redirects=True)
+        self.assertTrue(b'You need to login first.' in response.data)
+"""
+
 if __name__ == '__main__':
     unittest.main()
