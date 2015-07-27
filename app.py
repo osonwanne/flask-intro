0d052f3c2fed84f42ca8c2ce53046d2caffd9d13
osonwanne <nosonwan@gmail.com>
2015-07-26 18:39:21 -0700

    login_required, session

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template, redirect, url_for, request, session
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps
 
 # create the application object
 app = Flask(__name__)
 
+# config
 app.secret_key = "my precious"
+
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+        return wrap
 
 # use decorators to link the function to a url
 @app.route('/')
+@login_required
 def home():
-    return "Hello, Chance!" #return a string
+   # return "Hello, Chance!" #return a string
+    return render_template("index.html") # render a template
 
 @app.route('/welcome')
 def welcome():
@@ -23,12 +38,15 @@ def login():
             error = 'Invalid credentials. Please try again.'
         else:
             session['logged_in'] = True
+            flash('You were just logged in!')
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
 
 @app.route('/logout')
+@login_required
 def logout():
     session.pop('logged_in', None)
+    flash('You were just logged out!')
     return redirect(url_for('welcome'))
 
 
